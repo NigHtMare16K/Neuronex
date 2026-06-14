644e8fb2f09f97e4ac1a74dc573c8e10204fcb06
NigHtMare16K <kashufc247@gmail.com>
2026-06-14 12:40:02 +0530

    Did some functional changes

---
+++ output/alert_handler.py
@@ -0,0 +1,11 @@
+# output/alert_handler.py
+
+class AlertHandler:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def update(self, *args, **kwargs):
+        pass
+
+    def close(self):
+        pass
